c924755fb838b4a12b352a5c5b3ad440a4fdf31e
2509997n <2509997n@student.gla.ac.uk>
2023-03-08 15:52:16 +0000

    added h1 format, counter & password criteria

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function MyForm() {
   const [inputs, setInputs] = useState({});
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -16,26 +18,38 @@ function MyForm() {
     alert(inputs);
   }
 
+  useEffect(() => {
+    setCalculation(() => count);
+  }, [count]);
+
   return (
+    <>
+    <h1>Set up your password</h1>
+    <p>You must include:</p>
+    <li>Minimum of 8 characters</li>
+    <li>At least 1 number</li>
+    <li>At least 1 special character</li>
+    <li>At least 1 emoji</li>
     <form className="App" onSubmit={handleSubmit}>
       <label>Enter your password:
       <input 
         type="text" 
         name="password" 
         value={inputs.password || ""} 
-        onChange={handleChange}
-      />
+        onChange={handleChange}/>
       </label>
       <label>Confirm password:
         <input 
           type="text" 
           name="confirmPassword" 
           value={inputs.confirmPassword || ""} 
-          onChange={handleChange}
-        />
+          onChange={handleChange}/>
         </label>
         <input type="submit" />
+        <button onClick={() => setCount((c) => c + 1)}>Enter</button>
+        <p>Attempts: {calculation}</p>
     </form>
+    </>
   )
 }
 
